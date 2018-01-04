ce8843de14fa2909b64849fdfe8ccd99f2eedfb1
John <ionutciuta24@gmail.com>
2018-01-04 16:22:49 +0200

    OAuth API setup

---
+++ oauth2-api/src/main/java/com/sci/oauth2api/Oauth2ApiApplication.java
@@ -0,0 +1,12 @@
+package com.sci.oauth2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ApiApplication.class, args);
+	}
+}
